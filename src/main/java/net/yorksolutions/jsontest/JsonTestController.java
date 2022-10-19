4253d20e1991c364d047e61bdac6e22b8d25a2ab
Vicente <vicente.maldonado127@gmail.com>
2022-10-19 16:36:32 -0500

    completed get IP

--- src/main/java/net/yorksolutions/jsontest/JsonTestController.java
+++ src/main/java/net/yorksolutions/jsontest/JsonTestController.java
@@ -1,9 +1,39 @@
 package net.yorksolutions.jsontest;
 
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+
+@RestController
 public class JsonTestController {
 
+    //    @ResponseBody
+//    @GetMapping ("/geturl")
+//    public String getURLValue(HttpServletRequest request){
+//        //get URL value here which should be in this case, for instance if urlid
+//        //is 1 in request then  "/my/absolute/url/1/tests"
+//        String test = request.getRequestURI();
+//        return test;
+//    }
+
+    private JsonTestService jsonTestService;
+
     public JsonTestController(JsonTestService jsonTestService) {
+        this.jsonTestService = jsonTestService;
+    }
 
+    @GetMapping("/IP")
+    public HashMap getIP() throws UnknownHostException {
+        HashMap IP = new HashMap();
+        IP.put("IP",this.jsonTestService.getIP());
+        return IP;
+    }
 
+    @GetMapping("/get_headers")
+    public HashMap getHeader() {
+        return this.jsonTestService.getHeaders();
     }
 }
--- src/main/java/net/yorksolutions/jsontest/JsonTestService.java
+++ src/main/java/net/yorksolutions/jsontest/JsonTestService.java
@@ -1,7 +1,31 @@
 package net.yorksolutions.jsontest;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+import java.util.HashMap;
+
+@Service
 public class JsonTestService {
 
+ String test = "";
 
+ public String getIP() throws UnknownHostException {
+  try {
+   InetAddress local = InetAddress.getLocalHost();
+   return local.getHostAddress();
+  } catch (Exception ex) {
+   System.out.println(ex.getMessage());
+  }
+ return this.test;
+ }
+
+ public HashMap getHeaders() {
+ HashMap headers = new HashMap<>();
+ return headers;
+ }
 
 }
